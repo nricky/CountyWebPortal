49424c645e293c9f3a47ce11211f2314a4f478e7
harrymule <harrisonmule@gmail.com>
2014-05-16 12:08:15 +0300

    sample of the final backend

---
+++ mysite/_config.php
@@ -0,0 +1,17 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'silverstripe',
+	"password" => 'root',
+	"database" => 'silverstripe_ncc1',
+	"path" => '',
+);
+
+// Set the site locale
+i18n::set_locale('en_US');
